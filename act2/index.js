07752e69e0a66c22f8d2f58641d29401913ec932
jhedez <80602446+jhedez@users.noreply.github.com>
2021-07-02 06:50:59 -0500

    Add files via upload

---
+++ act2/index.js
@@ -0,0 +1,54 @@
+
+ // Scene + Camera + Renderer
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, .1, 1000 )
+const renderer = new THREE.WebGLRenderer({ antialias: true})
+
+renderer.setSize( window.innerWidth, window.innerHeight )
+// sets renderer background color
+renderer.setClearColor("#ffffff")
+document.body.appendChild( renderer.domElement )
+camera.position.z = 6
+
+// resize canvas on resize window
+window.addEventListener( 'resize', () => {
+	let width = window.innerWidth
+	let height = window.innerHeight
+	renderer.setSize( width, height )
+	camera.aspect = width / height
+	camera.updateProjectionMatrix()
+})
+
+// basic cube
+var geometry = new THREE.SphereGeometry( 1.5, 15, 4 )
+var material = new THREE.MeshStandardMaterial( { color: 0xddd1, flatShading: true, metalness: 0, roughness: 1 })
+var cube = new THREE.Mesh ( geometry, material )
+scene.add( cube )
+
+// wireframe cube
+var geometry = new THREE.BoxGeometry( 3, 3, 3)
+var material = new THREE.MeshBasicMaterial( {
+	color: "#000000", wireframe: true, transparent: false
+})
+var wireframeCube = new THREE.Mesh ( geometry, material )
+scene.add( wireframeCube )
+
+// ambient light
+var ambientLight = new THREE.AmbientLight ( 0xffffff, 0.2)
+scene.add( ambientLight )
+
+// point light
+var pointLight = new THREE.PointLight( 0xffffff, 1 );
+pointLight.position.set( 25, 50, 25 );
+scene.add( pointLight );
+
+
+function animate() {
+	requestAnimationFrame( animate )
+	cube.rotation.x += 1.04;
+	cube.rotation.y += 1.04;
+	wireframeCube.rotation.x -= 0.01;
+	wireframeCube.rotation.y -= 0.01;
+	renderer.render( scene, camera )
+}
+animate()
