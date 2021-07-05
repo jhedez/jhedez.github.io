eb152a9a1c653d1cac6b06287419e88750bc2c89
Jordy Hernández Sánchez <80602446+jhedez@users.noreply.github.com>
2021-07-05 06:57:45 -0500

    make it better

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 // Make connection
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('https://hdz-jordy-act4.herokuapp.com/');
 
 // Query DOM
 var message = document.getElementById('message'),
